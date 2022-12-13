78883a7c5a61a75630d3a3bd274dcdb0e18c48d7
sanny <38938782+sannyschulz@users.noreply.github.com>
2022-12-13 20:29:44 +0100

    refactoring: use ptf functions from hermes

--- src/ptf_testing/go.mod
+++ src/ptf_testing/go.mod
@@ -2,4 +2,11 @@ module github.com/zalf-rpm/Hermes2Go/src/ptf_testing
 
 go 1.17
 
-require github.com/go-echarts/go-echarts/v2 v2.2.4
+require (
+	github.com/go-echarts/go-echarts/v2 v2.2.4
+	github.com/zalf-rpm/Hermes2Go/hermes v0.0.0-20221208175533-78d6bd21bdc0
+)
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
+
+replace github.com/zalf-rpm/Hermes2Go/hermes => ../../hermes
--- src/ptf_testing/ptf_functions.go
+++ src/ptf_testing/ptf_functions.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"io"
-	"math"
 	"net/http"
 	"os"
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/components"
 	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/zalf-rpm/Hermes2Go/hermes"
 )
 
 func main() {
@@ -138,43 +138,50 @@ func genSurface3dFC(cGehalt float64, ptf int) ([]opts.Chart3DData, []opts.Chart3
 		for ton := 1; ton < 99; ton++ {
 			if sand+ton < 100 {
 				schluf := float64(100 - sand - ton)
+				var fc, pwp float64
 				if ptf == 4 {
-					ix := -0.837531 + 0.430183*cGehalt
-					ix2 := math.Pow(ix, 2)
-					ix3 := math.Pow(ix, 3)
-					yps := -1.40744 + 0.0661969*float64(ton)
-					yps2 := math.Pow(yps, 2)
-					yps3 := math.Pow(yps, 3)
-					zet := -1.51866 + 0.0393284*float64(sand)
-					zet2 := math.Pow(zet, 2)
-					zet3 := math.Pow(zet, 3)
+					fc, pwp = hermes.PTF4(cGehalt, float64(ton), float64(sand))
+					// ix := -0.837531 + 0.430183*cGehalt
+					// ix2 := math.Pow(ix, 2)
+					// ix3 := math.Pow(ix, 3)
+					// yps := -1.40744 + 0.0661969*float64(ton)
+					// yps2 := math.Pow(yps, 2)
+					// yps3 := math.Pow(yps, 3)
+					// zet := -1.51866 + 0.0393284*float64(sand)
+					// zet2 := math.Pow(zet, 2)
+					// zet3 := math.Pow(zet, 3)
 
-					fc := (29.7528 + 10.3544*(0.0461615+0.290955*ix-0.0496845*ix2+0.00704802*ix3+0.269101*yps-0.176528*ix*yps+0.0543138*ix2*yps+0.1982*yps2-0.060699*yps3-0.320249*zet-0.0111693*ix2*zet+0.14104*yps*zet+0.0657345*ix*yps*zet-0.102026*yps2*zet-0.04012*zet2+0.160838*ix*zet2-0.121392*yps*zet2-0.061667*zet3)) / 100
-					pwp := (14.2568 + 7.36318*(0.06865+0.108713*ix-0.0157225*ix2+0.00102805*ix3+0.886569*yps-0.223581*ix*yps+0.0126379*ix2*yps+0.0135266*ix*yps2-0.0334434*yps3-0.0535182*zet-0.0354271*ix*zet-0.00261313*ix2*zet-0.154563*yps*zet-0.0160219*ix*yps*zet-0.0400606*yps2*zet-0.104875*zet2*0.0159857*ix*zet2-0.0671656*yps*zet2-0.0260699*zet3)) / 100
+					// fc := (29.7528 + 10.3544*(0.0461615+0.290955*ix-0.0496845*ix2+0.00704802*ix3+0.269101*yps-0.176528*ix*yps+0.0543138*ix2*yps+0.1982*yps2-0.060699*yps3-0.320249*zet-0.0111693*ix2*zet+0.14104*yps*zet+0.0657345*ix*yps*zet-0.102026*yps2*zet-0.04012*zet2+0.160838*ix*zet2-0.121392*yps*zet2-0.061667*zet3)) / 100
+					// pwp := (14.2568 + 7.36318*(0.06865+0.108713*ix-0.0157225*ix2+0.00102805*ix3+0.886569*yps-0.223581*ix*yps+0.0126379*ix2*yps+0.0135266*ix*yps2-0.0334434*yps3-0.0535182*zet-0.0354271*ix*zet-0.00261313*ix2*zet-0.154563*yps*zet-0.0160219*ix*yps*zet-0.0400606*yps2*zet-0.104875*zet2*0.0159857*ix*zet2-0.0671656*yps*zet2-0.0260699*zet3)) / 100
 
-					dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
-					dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
+					// dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
+					// dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
 				} else if ptf == 3 {
 
 					// PTF by Batjes for pF 1.7
-					fc := (0.6681*float64(ton) + 0.2614*schluf + 2.215*cGehalt) / 100
-					pwp := (0.3624*float64(ton) + 0.117*schluf + 1.6054*cGehalt) / 100
+					fc, pwp = hermes.PTF3(cGehalt, float64(ton), float64(schluf))
+					// fc := (0.6681*float64(ton) + 0.2614*schluf + 2.215*cGehalt) / 100
+					// pwp := (0.3624*float64(ton) + 0.117*schluf + 1.6054*cGehalt) / 100
 
-					dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
-					dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
+					// dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
+					// dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
 				} else if ptf == 1 {
-					fc := 0.2449 - 0.1887*(1/(cGehalt+1)) + 0.004527*float64(ton) + 0.001535*schluf + 0.001442*schluf*(1/(cGehalt+1)) - 0.0000511*schluf*float64(ton) + 0.0008676*float64(ton)*(1/(cGehalt+1))
-					pwp := 0.09878 + 0.002127*float64(ton) - 0.0008366*schluf - 0.0767*(1/(cGehalt+1)) + 0.00003853*schluf*float64(ton) + 0.00233*schluf*(1/(cGehalt+1)) + 0.0009498*schluf*(1/(cGehalt+1))
 
-					dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
-					dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
+					fc, pwp = hermes.PTF1(cGehalt, float64(ton), float64(schluf))
+					// fc := 0.2449 - 0.1887*(1/(cGehalt+1)) + 0.004527*float64(ton) + 0.001535*schluf + 0.001442*schluf*(1/(cGehalt+1)) - 0.0000511*schluf*float64(ton) + 0.0008676*float64(ton)*(1/(cGehalt+1))
+					// pwp := 0.09878 + 0.002127*float64(ton) - 0.0008366*schluf - 0.0767*(1/(cGehalt+1)) + 0.00003853*schluf*float64(ton) + 0.00233*schluf*(1/(cGehalt+1)) + 0.0009498*schluf*(1/(cGehalt+1))
+
+					// dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
+					// dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
 				} else if ptf == 2 {
 					// PTF by Batjes for pF 2.5
-					fc := (0.46*float64(ton) + 0.3045*schluf + 2.0703*cGehalt) / 100
-					pwp := (0.3624*float64(ton) + 0.117*schluf + 1.6054*cGehalt) / 100
-					dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
-					dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
+					fc, pwp = hermes.PTF2(cGehalt, float64(ton), float64(schluf))
+					// fc := (0.46*float64(ton) + 0.3045*schluf + 2.0703*cGehalt) / 100
+					// pwp := (0.3624*float64(ton) + 0.117*schluf + 1.6054*cGehalt) / 100
+
 				}
+				dataFC = append(dataFC, [4]interface{}{sand, ton, fc * 100, fc > pwp})
+				dataPWP = append(dataPWP, [3]interface{}{sand, ton, pwp * 100})
 			}
 		}
 	}
