a724fbac16fd107b07c0fe92f5211919f73d18f8
Sanny <38938782+sannyschulz@users.noreply.github.com>
2024-02-06 00:29:15 +0100

    Add script to convert all crop param files to new format

---
+++ src/cropfileconverter/convert_all.sh
@@ -0,0 +1,31 @@
+# !/bin/bash -x
+
+# Convert all files in the directory to the new format
+
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_0.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_00.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_000.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_0000.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_chr.zr 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_i.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_ii.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM_iii.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.AA 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.GR 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.K 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.LUP 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.OA 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.OEL 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.ORH 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.PH 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.SE 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.SM 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.SOY 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.SW 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.TR 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.WG 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.WR 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.WRA 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.WRC 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.WW 
+./cropfileconverter.exe -input ../../examples/parameter/PARAM.ZR
