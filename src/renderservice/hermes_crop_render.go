a7828a7ca7c5ef33eee6e29f23719fd7a9bfe347
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-04-05 18:50:53 +0200

    Update hermes_crop_render.go to include new charts and markers for dev stage values

--- src/renderservice/hermes_crop_render.go
+++ src/renderservice/hermes_crop_render.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/components"
@@ -15,14 +16,16 @@ func cropdebughttpserver(w http.ResponseWriter, _ *http.Request) {
 	page := components.NewPage()
 	keys := extractSortedKeys()
 	dates := keysAsDate(Kalender, keys)
-	errKeys := generateErrorItems(keys)
+	stageKeys, stageValues := generateDevStageItems(keys)
 
 	// GEHMIN
 	// GEHMAX
 	// PE
 	page.AddCharts(
-		lineMultiGEHALT(keys, errKeys, dates),
-		lineMultiUptake(keys, errKeys, dates),
+		lineMultiGEHALT(keys, stageKeys, stageValues, dates),
+		lineMultiNUptake(keys, stageKeys, stageValues, dates),
+		lineMultiWaterUptake(keys, stageKeys, stageValues, dates),
+		lineMultiOrganBioMass(keys, stageKeys, stageValues, dates),
 	)
 
 	page.Render(w)
@@ -30,16 +33,35 @@ func cropdebughttpserver(w http.ResponseWriter, _ *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
 	page.Render(io.MultiWriter(f))
 
 }
 
-func lineMultiGEHALT(keys, errKeys []int, dates []string) *charts.Line {
+func generateDevStageItems(keys []int) ([]int, []int) {
+	globalHandler.mux.Lock()
+	listDevStageChange := []int{}
+	listDevStageChangeValue := []int{}
+
+	currentIntWICK := -1
+	for _, key := range keys {
+		tag := globalHandler.receivedDumps[key].Global.INTWICK.Index
+		if tag >= 0 && tag != currentIntWICK {
+			listDevStageChange = append(listDevStageChange, key)
+			listDevStageChangeValue = append(listDevStageChangeValue, tag+1)
+			currentIntWICK = tag
+		}
+	}
+	globalHandler.mux.Unlock()
+	return listDevStageChange, listDevStageChangeValue
+}
+
+func lineMultiGEHALT(keys, devStage, devStageVal []int, dates []string) *charts.Line {
 
 	line := makeMultiLine("GEHALT")
 
 	line.SetXAxis(dates).
-		AddSeries("GEHMIN", generateGehMinItems(keys), errorMarker(errKeys, 23)).
+		AddSeries("GEHMIN", generateGehMinItems(keys), stageMarker(devStage, devStageVal)).
 		AddSeries("GEHMAX", generateGehMaxItems(keys)).
 		AddSeries("WUGEH", generateWuGehItems(keys)).
 		AddSeries("GEHOB", generateGehobItems(keys))
@@ -47,14 +69,56 @@ func lineMultiGEHALT(keys, errKeys []int, dates []string) *charts.Line {
 	return line
 }
 
-func lineMultiUptake(keys, errKeys []int, dates []string) *charts.Line {
+func lineMultiNUptake(keys, devStage, devStageVal []int, dates []string) *charts.Line {
 
-	line := makeMultiLine("Uptake")
+	line := makeMultiLine("Daily N Uptake")
 
 	line.SetXAxis(dates).
-		AddSeries("PE", generatePEItems(keys), errorMarker(errKeys, 23))
+		AddSeries("PE", generatePEItems(keys), stageMarker(devStage, devStageVal))
 
 	return line
+}
+
+func lineMultiWaterUptake(keys, devStage, devStageVal []int, dates []string) *charts.Line {
+
+	line := makeMultiLine("Daily Water Uptake")
+
+	line.SetXAxis(dates).
+		AddSeries("TP", generateTPItems(keys), stageMarker(devStage, devStageVal))
+
+	return line
+}
+func lineMultiOrganBioMass(keys, devStage, devStageVal []int, dates []string) *charts.Line {
+
+	line := makeMultiLine("Daily Water Uptake")
+
+	line.SetXAxis(dates).
+		AddSeries("WORG0", generateWORGItems(keys, 0), stageMarker(devStage, devStageVal)).
+		AddSeries("WORG1", generateWORGItems(keys, 1)).
+		AddSeries("WORG2", generateWORGItems(keys, 2)).
+		AddSeries("WORG3", generateWORGItems(keys, 3)).
+		AddSeries("WORG4", generateWORGItems(keys, 4)).
+		AddSeries("HARVEST", generateYieldItems(keys))
+
+	return line
+}
+
+func stageMarker(stageKeys, values []int) charts.SeriesOpts {
+	dates := keysAsDate(Kalender, stageKeys)
+
+	marker := make([]opts.MarkPointNameCoordItem, 0, len(dates))
+	for idx, date := range dates {
+		// stage index as name
+		name := strconv.Itoa(values[idx])
+		marker = append(marker, opts.MarkPointNameCoordItem{
+			Name:       name,
+			Coordinate: []interface{}{date, 0},
+			Label:      &opts.Label{Show: true, Color: "white", Position: "inside", Formatter: "{b}"},
+		})
+	}
+	options := charts.WithMarkPointNameCoordItemOpts(marker...)
+
+	return options
 }
 
 // max Gehalt
@@ -120,6 +184,47 @@ func generatePEItems(keys []int) []opts.LineData {
 		}
 
 		items = append(items, opts.LineData{Value: uptake})
+	}
+	globalHandler.mux.Unlock()
+	return items
+}
+
+func generateTPItems(keys []int) []opts.LineData {
+	globalHandler.mux.Lock()
+	items := make([]opts.LineData, 0, len(keys))
+
+	for _, key := range keys {
+		wurzLen := globalHandler.receivedDumps[key].Global.WURZ
+		uptake := 0.0
+		for i := 0; i < wurzLen; i++ {
+			uptake = uptake + globalHandler.receivedDumps[key].Global.TP[i]
+		}
+
+		items = append(items, opts.LineData{Value: uptake})
+	}
+	globalHandler.mux.Unlock()
+	return items
+}
+
+func generateWORGItems(keys []int, organIndex int) []opts.LineData {
+	globalHandler.mux.Lock()
+	items := make([]opts.LineData, 0, len(keys))
+
+	for _, key := range keys {
+		val := globalHandler.receivedDumps[key].Global.WORG[organIndex]
+		items = append(items, opts.LineData{Value: val})
+	}
+	globalHandler.mux.Unlock()
+	return items
+}
+
+func generateYieldItems(keys []int) []opts.LineData {
+	globalHandler.mux.Lock()
+	items := make([]opts.LineData, 0, len(keys))
+
+	for _, key := range keys {
+		val := globalHandler.receivedDumps[key].Global.HARVEST
+		items = append(items, opts.LineData{Value: val})
 	}
 	globalHandler.mux.Unlock()
 	return items
