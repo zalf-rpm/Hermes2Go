f26c3b368893cd30da320226aba4f108a3aa03fb
Sanny <38938782+sannyschulz@users.noreply.github.com>
2025-02-11 20:21:57 +0100

    fix reading files

--- src/climatefileconverter/main.go
+++ src/climatefileconverter/main.go
@@ -10,6 +10,8 @@ func main() {
 	inPtr := flag.String("in", "SEH-weather-new.csv", "input file name")
 	outPtr := flag.String("out", "SEH-weather-new.w6d", "out file name")
 
+	flag.Parse()
+
 	err := ConvertFileMonicaToCZ(*inPtr, *outPtr)
 	if err != nil {
 		fmt.Println(err)
